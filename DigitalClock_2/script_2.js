e3bf0caac75c694cb55fcbb49ea1eb2a1a9fbd21
Patipoll <ezboomgg123@gmail.com>
2023-05-10 14:31:20 +0700

    DigitalClock_commit

---
+++ DigitalClock_2/script_2.js
@@ -0,0 +1,15 @@
+const time = document.getElementById('time')
+
+setInterval(() => {
+    let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+if (minutes < 10) {
+    minutes = '0' + minutes;
+}
+if (seconds < 10) {
+    seconds = '0' + seconds;
+}
+time.textContent = hours + ':' + minutes + ':' + seconds + ' น.'
+})
